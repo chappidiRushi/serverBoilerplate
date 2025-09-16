347375b984e7e99b9e6afd6ca3b053d02c8eef5c
chappidiRushi <chappidi.rushi@gmail.com>
2025-09-16 11:24:11 +0530

    deleted migrations

--- src/db/migrations/0001_yellow_darwin.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "User" RENAME TO "user";--> statement-breakpoint
-ALTER TABLE "Notification" DROP CONSTRAINT "Notification_userId_fkey";
---> statement-breakpoint
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."user"("userId") ON DELETE no action ON UPDATE no action;
