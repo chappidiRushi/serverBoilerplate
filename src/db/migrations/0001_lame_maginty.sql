bcb329806b6d303fbc01dfa46321de6971b5d296
chappidiRushi <chappidi.rushi@gmail.com>
2025-09-20 17:11:59 +0530

    changes

--- src/db/migrations/0001_lame_maginty.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "plant_category" ALTER COLUMN "createdAt" SET DATA TYPE timestamp (3) with time zone;--> statement-breakpoint
-ALTER TABLE "plant_category" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE "plant_category" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp (3) with time zone;--> statement-breakpoint
-ALTER TABLE "plant_category" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE "plant_category" ALTER COLUMN "deletedAt" SET DATA TYPE timestamp (3) with time zone;
